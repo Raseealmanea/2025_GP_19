b994dab8bf1f444138eb531e3d315676a43ad5a4
Dalia1003 <dalia.alsub@gmail.com>
2025-11-27 19:59:51 +0300

    Update Initialization.py

--- ouwn_website/firebase/Initialization.py
+++ ouwn_website/firebase/Initialization.py
@@ -1,11 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Path to your key file
-cred = credentials.Certificate(r"C:\MAMP\htdocs\ouwn_website\serviceAccountKey.json")
 
-# Initialize only once
 if not firebase_admin._apps:
+    cred = credentials.Certificate(r"C:\MAMP\htdocs\ouwn_website\serviceAccountKey.json")
     firebase_admin.initialize_app(cred)
 
 # Get Firestore client
